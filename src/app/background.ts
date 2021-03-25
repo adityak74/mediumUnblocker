1d8142585c15d4b33b1c46d2742d2711ce8b7a6e
Aditya Karnam <akarnam37@gmail.com>
2021-03-24 21:15:35 -0500

    chore: add support for more domains

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,17 +1,21 @@
-chrome.runtime.onMessage.addListener((_, sender, sendResponse) => {
+const supportedWebsiteUrls = ['https://medium.com', 'https://uxdesign.cc'];
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     var currentTabUrl = new URL(sender.tab.url);
     // clear cookies and reload
-    chrome.cookies.getAll({ url: 'https://medium.com' },function (cookie){
-        var allCookieInfo = [];
-        for(var i=0; i < cookie.length; i++) {
-            allCookieInfo.push(JSON.stringify(cookie[i]));
-            chrome.cookies.remove({
-                url: "" + currentTabUrl.protocol + "//" + currentTabUrl.hostname + cookie[i].path,
-                name: cookie[i].name,
-            });
-        }
-        chrome.tabs.update(sender.tab.id, { url: sender.tab.url });
-        sendResponse({ clearedCookies: true });
-    });
+    for (var i = 0; i < supportedWebsiteUrls.length; i += 1) {
+        chrome.cookies.getAll({ url: supportedWebsiteUrls[i] }, function (cookie) {
+            var allCookieInfo = [];
+            for(var i=0; i < cookie.length; i++) {
+                allCookieInfo.push(JSON.stringify(cookie[i]));
+                chrome.cookies.remove({
+                    url: "" + currentTabUrl.protocol + "//" + currentTabUrl.hostname + cookie[i].path,
+                    name: cookie[i].name,
+                });
+            }
+            chrome.tabs.update(sender.tab.id, { url: sender.tab.url });
+            sendResponse({ clearedCookies: true });
+        });
+    }
     return true;
 });
